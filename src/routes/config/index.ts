b66531a5759e825108e35e29fcfebc1fe7039e88
Danny A. Mosquera <danny.mosquera@dale.com>
2026-07-29 14:52:40 -0500

    feat(legal): add Privacy Policy and Terms of Service pages with routing configuration

--- src/routes/config/index.ts
+++ src/routes/config/index.ts
@@ -3,14 +3,19 @@ export const routes = {
     prefix: "/",
     routes: {
       home: "",
-      terms: "terms-of-service",
-      privacy: "policy-of-privacy",
       auth: {
         prefix: "auth",
         routes: {
           sign_in: "/sign-in",
           get_started: "/get-started",
           forgot_password: "/forgot-password",
+        },
+      },
+      legal: {
+        prefix: "legal",
+        routes: {
+          terms: "/terms-of-service",
+          privacy: "/policy-of-privacy",
         },
       },
     },
